83fc272a47010a85a0dc326aa3a375bd91da83d6
villuette <maksim.yarusov15092003@gmail.com>
2023-10-12 17:38:05 +0500

    few buttons and login via google only

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
-
+import { SignIn, SignOutButton, SignedOut, UserButton } from "@clerk/nextjs";
+import "./main.css";
+import Sidebar from "@/components/navigation/navigation-sidebar/Sidebar";
+import Header from "@/components/navigation/header/Header";
+import {
+  Button,
+  ButtonGroup,
+  IconButton,
+  IconButtonClasses,
+} from "@mui/material";
+import { AccountCircle, Assignment, Logout } from "@mui/icons-material";
 
 export default function Home() {
-  return(
-    <div>Home</div>
-  )
+  return (
+    <div className="h-full">
+      <Header>
+        <Button color="info" startIcon={<AccountCircle />}>
+          Friends
+        </Button>
+        <Button color="success" startIcon={<Assignment/>}>Notes</Button>
+      </Header>
+      <Sidebar />
+    </div>
+  );
 }
--- components/navigation/navigation-sidebar/Sidebar.tsx
+++ components/navigation/navigation-sidebar/Sidebar.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className='h-full bg-blue-800 w-1/5 inline-block text-white'>Sidebar</div>
   )
 }
